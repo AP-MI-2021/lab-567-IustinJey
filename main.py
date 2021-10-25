0d6d5995a3b3d55bb6bb6f628d718f65402b135c
Iustin Hociung <justinhociun@gmail.com>
2021-10-25 17:09:53 +0300

    Transformare din dictionar in lista

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from Domain.rezervare import creaza_rezervare, getId
 from Tests.AllTests import runAllTests
 from UserInterface.console import runMenu
 
 
 def main():
-    rezervare = creaza_rezervare(1, "Hociung Iustin", "Economy", 670, "Da")
-    print(getId(rezervare))
     runAllTests()
     lista = []
     runMenu(lista)
